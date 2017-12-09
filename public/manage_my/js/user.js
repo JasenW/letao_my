1eb2b4b6bf6d0c2b30b9def78e33b17b04bc4adb
jasonwang <596731020@qq.com>
2017-12-09 20:16:57 +0800

    点击启用禁用修改用户状态

--- public/manage_my/js/user.js
+++ public/manage_my/js/user.js
@@ -34,3 +34,33 @@ function getData() {
 
 // 默认先调用一次
 getData();
+
+
+// 点击按钮启用禁用修改状态
+$('tbody').on('click', 'button.btn', function () {
+
+    var myid = $(this).parent().data('id');
+    var myisDelete = $(this).parent().attr('data-isDelete');
+
+    if (myisDelete == 1) {
+        myisDelete = 0;
+
+    } else {
+        myisDelete = 1;
+    }
+
+    $.ajax({
+        url: '/user/updateUser',
+        type: 'post',
+        data: {
+            id: myid,
+            isDelete: myisDelete
+        },
+        success: function (backData) {
+            console.log(backData);
+
+            getData();
+        }
+    })
+
+})
